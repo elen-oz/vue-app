6b0d1f74010eb707a9d11b8525e402002e3d4fd5
elen-oz <ozerova.lenka@gmail.com>
2024-09-10 10:49:25 +0200

    init: initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<style scoped>
+  @import "https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css";
+</style>
