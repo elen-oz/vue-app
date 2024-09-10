dcd5a6817eb087bb0fb732a493af06c127c4b4d3
elen-oz <ozerova.lenka@gmail.com>
2024-09-10 17:04:14 +0200

    refactor: add types

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-const periods = ["Today", "This week", "This month"];
+import { ref } from "vue";
+
+const periods = ["Today", "This week", "This month"] as const;
+
+type Period = (typeof periods)[number];
+
+const selectedPeriod = ref<Period>("Today");
+
+const selectPeriod = (period: Period) => {
+  selectedPeriod.value = period;
+};
 </script>
 
 <template>
   <nav class="is-primary panel">
+    {{ selectedPeriod }}
     <span class="panel-tabs">
-      <a v-for="period of periods" :key="period">{{ period }}</a>
+      <a
+        v-for="period of periods"
+        :key="period"
+        @click="selectPeriod(period)"
+        >{{ period }}</a
+      >
     </span>
   </nav>
 </template>
