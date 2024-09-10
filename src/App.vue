f350f252617cf7d19f3fd32b1c9fe13cbe9846e2
elen-oz <ozerova.lenka@gmail.com>
2024-09-10 11:06:13 +0200

    feat: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-
+import Timeline from "./components/Timeline.vue";
 </script>
 
 <template>
-  <div>
-    hello
+  <div class="container">
+    <Timeline />
   </div>
 </template>
 
 <style scoped>
-  @import "https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css";
+@import "https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css";
 </style>
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-
+const periods = ["Today", "This week", "This month"];
 </script>
 
 <template>
-
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a v-for="period of periods" :key="period">{{ period }}</a>
+    </span>
+  </nav>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
